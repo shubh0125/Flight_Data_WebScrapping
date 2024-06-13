81b57ec068af8c2db2799942ddaaed944ed82be8
Shubh Almal <shubhalmal@gmail.com>
2024-06-13 13:24:49 -0400

    mmt scarpping

--- SearchEngine.py
+++ SearchEngine.py
@@ -1,5 +1,4 @@
-from open_browser import *
-
+import open_browser
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -14,7 +13,8 @@ class SearchEngine:
     
     def using_google(search_query):
         try:
-            browser = webdriver.Chrome()
+            browser = open_browser.initialize_chrome_driver()
+            # browser = webdriver.Chrome()
             browser.get("https://www.google.com")
             # Wait for the search box to be present and then interact with it
             search_box = WebDriverWait(browser, 10).until(
@@ -43,15 +43,12 @@ class SearchEngine:
 
         except Exception as e:
             print("An error occurred:", e)
-        finally:
-            # Clean up by closing the driver
-            browser.quit()
 
 
 
     def using_bing(search_query):
         # Initialize the Chrome driver
-        browser = webdriver.Chrome()
+        browser = open_browser.initialize_chrome_driver()
         try:
             # Navigate to Bing
             browser.get("https://www.bing.com/")
@@ -74,47 +71,10 @@ class SearchEngine:
             WebDriverWait(browser, 10).until(
                 EC.presence_of_element_located((By.TAG_NAME, "body"))
             )
+            return browser
 
         except TimeoutException:
             print("Timeout occurred while waiting for an element.")
         except NoSuchElementException:
             print("An element was not found.")
-        finally:
-            # Close the browser
-            browser.quit()
-
-
-    # def using_bing(search_query):
-    
-    #     browser = webdriver.Chrome()
-    #     browser.get("https://www.bing.com/")
-
-    #     search_box = WebDriverWait(browser, 10).until(
-    #             EC.presence_of_element_located((By.ID, "sb_form_q"))
-    #         )
-    #     search_box.send_keys(f"{search_query}" + Keys.RETURN)
-
-    #     first_link = WebDriverWait(browser, 10).until(
-    #             EC.presence_of_element_located((By.CSS_SELECTOR, "ol#b_results li.b_algo h2 a"))
-    #         )
-
-    #     # Extract the URL of the first search result
-    #     # first_link_url = first_link.get_attribute("href")
-        
-    #     first_link.click()
-        
-
-    #     WebDriverWait(browser, 10).until(
-    #             EC.presence_of_element_located((By.TAG_NAME, "body"))
-    #         )
-
-        # try:
-        #     while True:
-        #         sleep(1)
-        # except KeyboardInterrupt:
-        #     browser.quit()
-
-
-
-
-    using_bing("Make My Trip")
+        # finally:
